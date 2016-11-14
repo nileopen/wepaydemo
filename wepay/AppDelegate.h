8631f189b7fbb6dbd9ee763fc980078b6e0b9169
taotao <nile23@aliyun.com>
2016-11-14 14:57:06 +0800

    Initial Commit

---
+++ wepay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wepay
+//
+//  Created by taotao on 16/11/14.
+//  Copyright © 2016年 youja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wepay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wepay
+//
+//  Created by taotao on 16/11/14.
+//  Copyright © 2016年 youja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
